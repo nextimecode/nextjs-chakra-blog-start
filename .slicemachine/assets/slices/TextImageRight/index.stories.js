58f7905974f47d6ededfd0f927f062b4cc405635
Pedro Duarte <pedrohlduarte@gmail.com>
2022-04-09 21:29:59 -0300

    feat: initial congi prismic

---
+++ .slicemachine/assets/slices/TextImageRight/index.stories.js
@@ -0,0 +1,9 @@
+import MyComponent from '../../../../slices/TextImageRight';
+
+export default {
+  title: 'slices/TextImageRight'
+}
+
+
+export const _Default = () => <MyComponent slice={{"variation":"default","name":"Default","slice_type":"text_image_right","items":[],"primary":{"title":[{"type":"heading1","text":"Matrix visionary metrics","spans":[]}],"description":[{"type":"paragraph","text":"Aliqua anim deserunt laboris consectetur nulla deserunt commodo deserunt duis pariatur nostrud dolor. Magna velit aute laborum est veniam eu sunt ipsum et sunt ad cillum commodo consequat esse.","spans":[]}]},"id":"_Default"}} />
+_Default.storyName = 'Default'
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,28 @@
 import React from 'react'
 import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
+import Link from 'next/link'
+import { PrismicProvider } from '@prismicio/react'
+import { PrismicPreview } from '@prismicio/next'
+import { linkResolver, repositoryName } from '../prismicio'
 
 import theme from '../../theme/'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
+      <PrismicProvider
+        linkResolver={linkResolver}
+        internalLinkComponent={({ href, children, ...props }) => (
+          <Link href={href}>
+            <a {...props}>{children}</a>
+          </Link>
+        )}
+      >
+        <PrismicPreview repositoryName={repositoryName}>
+          <Component {...pageProps} />
+        </PrismicPreview>
+      </PrismicProvider>
     </ChakraProvider>
   )
 }
